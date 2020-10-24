dd79450d5e9178952747940b9fb0baede88f45f4
Torra Agustin <torra.agustinignacio@gmail.com>
2020-10-23 21:28:47 -0300

    New file

---
+++ src/shared/components/home/searchSection/SearchSection.tsx
@@ -0,0 +1,22 @@
+import React, { FC, useState } from "react";
+import { colors } from "../../../../browser/styles/colors";
+import { Container } from "../../../styled/Container";
+import SearchForm from "../../searchHeroes/SearchForm";
+
+const SearchSection: FC = () => {
+
+  return (
+    <Container
+      display="flex"
+      flexDir="column"
+      justifyContent="center"
+      alignItems="center"
+      bgColor={colors.primary}
+      color="#fff"
+    >
+      <SearchForm search={""} flexDir='column' />
+    </Container>
+  );
+};
+
+export default SearchSection;
---
+++ src/shared/components/searchHeroes/SearchForm.tsx
@@ -0,0 +1,52 @@
+import React, { FC, useRef, useState, useEffect } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { Form, Submit, Input } from "./searchForm.styles";
+
+type Props = { search: string; flexDir: string };
+
+const SearchForm: FC<Props & RouteComponentProps> = ({
+  search,
+  history,
+  flexDir,
+}) => {
+  const inputRef = useRef(null);
+  const [query, setQuery] = useState(search);
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    history.push({
+      pathname: "/search",
+      search: query,
+    });
+  };
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <>
+      <h1>Search heroes</h1>
+      <Form
+        display="flex"
+        flexDir={flexDir}
+        onSubmit={(e) => handleOnSubmit(e)}
+      >
+        <Input
+          ref={inputRef}
+          type="text"
+          placeholder="Insert hero name..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Submit
+          margin={flexDir === "column" ? ".5rem 0" : "1rem .25rem"}
+          type="submit"
+          value="Search"
+        />
+      </Form>
+    </>
+  );
+};
+
+export default withRouter(SearchForm);
