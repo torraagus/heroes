e246cf4174d750f6aab765b94597a6b557148495
Torra Agustin <torra.agustinignacio@gmail.com>
2020-10-24 15:02:55 -0300

    Add styled component Error

--- src/shared/components/searchHeroes/searchHeroes.styles.ts
+++ src/shared/components/searchHeroes/searchHeroes.styles.ts
@@ -2,23 +2,27 @@ import styled from "styled-components";
 import { colors } from "../../../browser/styles/colors";
 
 export const HomeBtn = styled.button`
-    border-radius: 10px;
-    border: none;
-    background-color: #eee;
-    color: ${colors.primary};
-    font-weight: bold;
-    text-transform: uppercase;
-    padding: .5rem 1rem;
-    font-size: 11px;
-    outline: none;
+  border-radius: 10px;
+  border: none;
+  background-color: #eee;
+  color: ${colors.primary};
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 0.5rem 1rem;
+  font-size: 11px;
+  outline: none;
 
-    :hover {
-        cursor: pointer;
-        opacity: 75%;
-    }
+  :hover {
+    cursor: pointer;
+    opacity: 75%;
+  }
 
-    :active {
-        cursor: pointer;
-        opacity: 90%;
-    }
+  :active {
+    cursor: pointer;
+    opacity: 90%;
+  }
+`;
+
+export const Error = styled.i`
+  color: red;
 `;
