a9aadbbc78db36e08eca6eb3effffde2bec6e06e
Torra Agustin <torra.agustinignacio@gmail.com>
2020-10-24 15:01:14 -0300

    Add flexWrap prop

--- src/shared/styled/Container.ts
+++ src/shared/styled/Container.ts
@@ -1,25 +1,27 @@
 import styled from "styled-components";
 
 type ContainerProps = {
-    width?: string;
-    height?: string;
-    display?: string;
-    flexDir?: string;
-    justifyContent?: string;
-    alignItems?: string;
-    bgColor?: string;
-    color?: string;
-    padding?: string;
-}
+  width?: string;
+  height?: string;
+  display?: string;
+  flexDir?: string;
+  justifyContent?: string;
+  alignItems?: string;
+  bgColor?: string;
+  color?: string;
+  padding?: string;
+  flexWrap?: string;
+};
 
 export const Container = styled.div<ContainerProps>`
-    width: ${({width}) => width ? width : '100vw'};
-    min-height: ${({height}) => height ? height : '100vh'};
-    display: ${({display}) => display ? display : 'block'};
-    flex-direction: ${({flexDir}) => flexDir && flexDir};
-    justify-content: ${({justifyContent}) => justifyContent && justifyContent};
-    align-items: ${({alignItems}) => alignItems && alignItems};
-    background-color: ${({bgColor}) => bgColor ? bgColor : "#fff"};
-    color: ${({color}) => color ? color : "#000"};
-    padding: ${({padding}) => padding && padding};
+  width: ${({ width }) => (width ? width : "100vw")};
+  min-height: ${({ height }) => (height ? height : "100vh")};
+  display: ${({ display }) => (display ? display : "block")};
+  flex-direction: ${({ flexDir }) => flexDir && flexDir};
+  justify-content: ${({ justifyContent }) => justifyContent && justifyContent};
+  align-items: ${({ alignItems }) => alignItems && alignItems};
+  background-color: ${({ bgColor }) => (bgColor ? bgColor : "#fff")};
+  color: ${({ color }) => (color ? color : "#000")};
+  padding: ${({ padding }) => padding && padding};
+  flex-wrap: ${({ flexWrap }) => flexWrap && flexWrap};
 `;
