a7079f3c928cf8c4253fb14afadb6ea101f1301f
Torra Agustin <torra.agustinignacio@gmail.com>
2020-10-24 23:27:27 -0300

    Add heroeDetails reducer to combineReducers

--- src/shared/redux/rootReducer.ts
+++ src/shared/redux/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import heroesReducer from './searchHeroes/searchHeroesReducer'
 import loadingReducer from './loadingBar/loadingReducer'
+import heroeDetailsReducer from './heroeDetails/heroeDetailsReducer'
 
 const rootReducer = combineReducers({
     heroes: heroesReducer,
-    loader: loadingReducer
+    loader: loadingReducer,
+    heroeDetails: heroeDetailsReducer
 });
 
 export default rootReducer;
