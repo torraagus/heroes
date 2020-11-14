88e318e244e6225e1018c767f4a549e5840fd3a8
Torra Agustin <torra.agustinignacio@gmail.com>
2020-11-14 18:14:48 -0300

    Update file

--- src/shared/components/heroImage/heroImage.test.tsx
+++ src/shared/components/heroImage/heroImage.test.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { assert, expect } from "chai";
 import { mount, render, shallow } from "enzyme";
-import { BrowserRouter as Router } from "react-router-dom";
 import HeroImage from "./HeroImage";
 import { Image } from "./heroImage.styles";
-import { HeroId, HeroName } from "../compareHeroes/compareHeroes.styles";
-import Powerstats from "../heroDetails/powerstats/Powerstats";
+import { Id, LoaderIcon, Name } from "../heroItem/heroItem.styles";
 
 describe("src/shared/components/HeroImage.tsx", () => {
   it("should render without errors", () => {
@@ -17,14 +15,23 @@ describe("src/shared/components/HeroImage.tsx", () => {
     expect(wrapper).exist;
   });
 
-  it("should render image", () => {
-    const children = (
-      <>
-        <HeroId>`id`</HeroId>
-        <HeroName>`name`</HeroName>
-        {/* <Powerstats powerstats={[]} /> */}
-      </>
-    );
+  it("should render Image component", () => {
+    const props = {
+      hero: {
+        id: "1",
+        name: "hero1",
+        image: {
+          url: "",
+        },
+      },
+      children: {},
+    };
+    const wrapper = mount(<HeroImage {...props} />);
+    expect(wrapper.find(Image)).to.have.length(1);
+    expect(wrapper.find(LoaderIcon)).to.have.length(1);
+  });
+
+  it("should show hero info after onLoad event", () => {
     const props = {
       hero: {
         id: "1",
@@ -34,9 +41,41 @@ describe("src/shared/components/HeroImage.tsx", () => {
             "https://www.superherodb.com/pictures2/portraits/10/100/1460.jpg",
         },
       },
-      children,
+      children: (
+        <>
+          <Id>#1</Id>
+          <Name>hero1</Name>
+        </>
+      ),
     };
     const wrapper = mount(<HeroImage {...props} />);
-    expect(wrapper.find(Image)).to.have.length(1);
+    wrapper.find(Image).simulate("load");
+    expect(wrapper.find(LoaderIcon)).to.have.length(0);
+    expect(wrapper.find(Id)).to.have.length(1);
+    expect(wrapper.find(Name)).to.have.length(1);
+  });
+
+  it("should show hero info after onError event", () => {
+    const props = {
+      hero: {
+        id: "1",
+        name: "hero1",
+        image: {
+          url:
+            "https://www.superherodb.com/pictures2/portraits/10/100/1460.jpg",
+        },
+      },
+      children: (
+        <>
+          <Id>#1</Id>
+          <Name>hero1</Name>
+        </>
+      ),
+    };
+    const wrapper = mount(<HeroImage {...props} />);
+    wrapper.find(Image).simulate("error");
+    expect(wrapper.find(LoaderIcon)).to.have.length(0);
+    expect(wrapper.find(Id)).to.have.length(1);
+    expect(wrapper.find(Name)).to.have.length(1);
   });
 });
