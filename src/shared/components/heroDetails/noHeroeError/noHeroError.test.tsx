202521a861c484c6eef5df240db11c3c53ebd47b
Torra Agustin <torra.agustinignacio@gmail.com>
2020-11-13 23:11:40 -0300

    Update file

--- src/shared/components/heroDetails/noHeroeError/noHeroError.test.tsx
+++ src/shared/components/heroDetails/noHeroeError/noHeroError.test.tsx
@@ -3,14 +3,17 @@ import { assert, expect } from "chai";
 import { mount, render, shallow } from "enzyme";
 import NoHeroError from "./NoHeroError";
 import { Button, Title } from "./noHeroError.styles";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import sinon from "sinon";
+import { createMemoryHistory } from "history";
 
 describe("src/shared/components/noHeroError.tsx", () => {
   it("should render without errors", () => {
     const wrapper = shallow(
-      <Router>
+      <BrowserRouter>
         <NoHeroError title="Error" desc="Error description" />
-      </Router>
+      </BrowserRouter>
     );
     expect(wrapper).exist;
   });
@@ -18,20 +21,32 @@ describe("src/shared/components/noHeroError.tsx", () => {
   it("should render a Title", () => {
     const props = { title: "Error", desc: "Error description" };
     const wrapper = mount(
-      <Router>
+      <BrowserRouter>
           <NoHeroError {...props} />
-      </Router>
+          </BrowserRouter>
     );
     expect(wrapper.childAt(0).find(Title)).to.have.length(1);
   });
 
   it("should render a Button", () => {
     const wrapper = mount(
-      <Router>
+      <BrowserRouter>
+        <NoHeroError title="Error" desc="Error description" showBtn />
+        </BrowserRouter>
+    );
+    expect(wrapper.childAt(0).find(Button)).to.have.length(1);
+  });
+  
+  it("should go home when press the Button", () => {
+    const historyObj = createMemoryHistory();
+    historyObj.location.pathname = "/heroes/888";
+    const pushSpy = sinon.spy(historyObj, "push");
+    const wrapper = mount(
+      <Router history={historyObj}>
         <NoHeroError title="Error" desc="Error description" showBtn />
       </Router>
     );
-	// wrapper.childAt(0).find(Button).simulate("click");
-    expect(wrapper.childAt(0).find(Button)).to.have.length(1);
+	  wrapper.childAt(0).find(Button).simulate("click");
+    sinon.assert.calledWith(pushSpy, "/");
   });
 });
